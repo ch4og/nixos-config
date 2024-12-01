0fcfee6567ba2524c026910abec54fed0c89ca22
ch4og <mitanick@ya.ru>
2024-12-01 23:22:02 +0300

    switch telegram back to xcb, laggy on wayland

--- user/ch/home/hyprland/autostart.nix
+++ user/ch/home/hyprland/autostart.nix
@@ -3,8 +3,8 @@
     "waybar"
     "wl-paste --watch cliphist store"
 
+    "[title;workspace 4 silent] env QT_QPA_PLATFORM=xcb telegram-desktop"
     "[workspace 2 silent] firefox"
-    "[title;workspace 4 silent] telegram-desktop"
     "[title;workspace 6 silent] discordcanary --disable-gpu-compositing"
     "[workspace 7 silent] spotify"
     "[title;workspace 10 silent] steam -silent"
