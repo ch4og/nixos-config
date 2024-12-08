c541a1108053dc34bfc4fa10ce7b52ad1c2f1c17
ch4og <mitanick@ya.ru>
2024-12-08 16:31:42 +0300

    switch back to vesktop temp

--- user/ch/home/hyprland/autostart.nix
+++ user/ch/home/hyprland/autostart.nix
@@ -5,7 +5,7 @@
 
     "[title;workspace 4 silent] env QT_QPA_PLATFORM=xcb telegram-desktop"
     "[workspace 2 silent] firefox"
-    "[title;workspace 6 silent] discordcanary --disable-gpu-compositing"
+    "[title;workspace 6 silent] vesktop --disable-gpu-compositing"
     "[workspace 7 silent] spotify"
     "[title;workspace 10 silent] steam -silent"
   ];
