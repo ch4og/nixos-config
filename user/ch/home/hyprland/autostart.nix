da7a6ba28913337d494a1b5e4e3ce353bf0ef1e8
ch4og <mitanick@ya.ru>
2024-12-10 23:59:08 +0300

    fix whitescreen on vesktop autostart (make sure that proxy is connected already)

--- user/ch/home/hyprland/autostart.nix
+++ user/ch/home/hyprland/autostart.nix
@@ -7,7 +7,7 @@
 
     "[title;workspace 4 silent] env QT_QPA_PLATFORM=xcb telegram-desktop"
     "[workspace 2 silent] firefox"
-    "[title;workspace 6 silent] vesktop --disable-gpu-compositing"
+    "[title;workspace 6 silent] sleep 10 && vesktop --disable-gpu-compositing"
     "[workspace 7 silent] spotify"
     "[title;workspace 10 silent] steam -silent"
   ];
