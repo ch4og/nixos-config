1ebf66a011e361ef1303c1c1c9f6b5af1c736c19
ch4og <mitanick@ya.ru>
2024-12-10 17:57:05 +0300

    add mic vban to second pc

--- user/ch/home/bin/vban.sh
+++ user/ch/home/bin/vban.sh
@@ -2,4 +2,5 @@
 #! nix-shell -i bash --impure
 #! nix-shell -p pulseaudio
 
-vban_receptor -i 10.42.0.253 -p 6980 -s winpc -b pulseaudio -q 0
+vban_receptor -i 10.42.0.253 -p 6980 -s winpc -b pulseaudio
+vban_emitter -i 10.42.0.253 -p 6980 -s G435_MIC -b alsa -r 48000
