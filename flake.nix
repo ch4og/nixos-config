e3d2eb198006283e9df046191f485df9e5362f1b
ch4og <mitanick@ya.ru>
2025-01-03 18:51:26 +0300

    update with temp switch to small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ch4og config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
