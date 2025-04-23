bff3e8e1fa77698c8a4323236419afe48b6f7bdd
ch4og <mitanick@ya.ru>
2025-04-23 17:34:20 +0300

    fix mimeapps fails rebuild

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # TODO:
 
-- [ ] Fix mimeapps.list to be always overwritten
 - [ ] Make README prettier
 - [ ] Add more documentation
 - [ ] Refactor and restructure code
@@ -10,5 +9,3 @@
 - [ ] Add Adwaita theme for GTK and QT (or any other uniform theme)
 - [ ] SDDM theme
 - [ ] Use [Stylix](https://github.com/danth/stylix) for theming?
-- [ ] Find why sometimes network dies and drops packets (still?)
-- [ ] Setup Pi-hole on NixOS on RPi
--- user/ch/home/defaultapps.nix
+++ user/ch/home/defaultapps.nix
@@ -1,21 +1,24 @@
 {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "application/json" = "firefox.desktop";
-      "application/pdf" = "firefox.desktop";
-      "application/x-xpinstall" = "firefox.desktop";
-      "application/xhtml+xml" = "firefox.desktop";
-      "text/html" = "firefox.desktop";
-      "text/xml" = "firefox.desktop";
-      "x-scheme-handler/http" = "firefox.desktop";
-      "x-scheme-handler/https" = "firefox.desktop";
-      "inode/directory" = "thunar.desktop";
-      "image/jpeg" = "org.gnome.eog.desktop";
-      "image/png" = "org.gnome.eog.desktop";
-      "image/svg+xml" = "org.gnome.eog.desktop";
-      "x-scheme-handler/tg" = "com.ayugram.desktop.desktop";
-      "x-scheme-handler/tonsite" = "com.ayugram.desktop.desktop";
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/json" = "firefox.desktop";
+        "application/pdf" = "firefox.desktop";
+        "application/x-xpinstall" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
+        "text/html" = "firefox.desktop";
+        "text/xml" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "inode/directory" = "thunar.desktop";
+        "image/jpeg" = "org.gnome.eog.desktop";
+        "image/png" = "org.gnome.eog.desktop";
+        "image/svg+xml" = "org.gnome.eog.desktop";
+        "x-scheme-handler/tg" = "com.ayugram.desktop.desktop";
+        "x-scheme-handler/tonsite" = "com.ayugram.desktop.desktop";
+      };
     };
+    configFile."mimeapps.list".force = true;
   };
 }
