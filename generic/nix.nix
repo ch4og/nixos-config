7c9f60c737c0484befdcef6eedbc83fc9527090e
ch4og <mitanick@ya.ru>
2025-04-27 16:11:59 +0300

    fix substiuters

--- generic/nix.nix
+++ generic/nix.nix
@@ -4,12 +4,15 @@
     settings = {
       substituters = [
         "https://nixos-bunny-proxy.cofob.dev" # mirror of cache.nixos.org
+        "https://nix-community.cachix.org" # nix-community
         "https://cache.garnix.io" # garnix
         "https://nix-gaming.cachix.org" # nix-gaming
         "https://ezkea.cachix.org" # aagl
         "https://hyprland.cachix.org" # hyprland
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" # cache.nixos.org
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" # nix-community
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" # garnix
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" # nix-gaming
         "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" # aagl
