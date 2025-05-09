a482d4ec9d608f22b537cfa50d2390ab0243b754
ch4og <mitanick@ya.ru>
2025-05-09 20:35:29 +0300

    add ssh config

---
+++ home/modules/ssh/default.nix
@@ -0,0 +1,25 @@
+_: {
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "csmpro.ru" = {
+        hostname = "csmpro.ru";
+        identityFile = "~/.ssh/csm_id_ed25519";
+        port = 2222;
+      };
+      "v.ch4og.com" = {
+        hostname = "v.ch4og.com";
+        identityFile = "~/.ssh/id_ed25519_vpn";
+        port = 2222;
+      };
+      "gitlab.com" = {
+        hostname = "gitlab.com";
+        identityFile = "~/.ssh/id_ed25519_ch4og";
+      };
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_ed25519_ch4og";
+      };
+    };
+  };
+}
