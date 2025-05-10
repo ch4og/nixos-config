c2e9a0adbd0825125f73c9f647b43e17a2304173
ch4og <mitanick@ya.ru>
2025-05-10 23:52:51 +0300

    add libvirtd/qemu/virt-manager

--- home/packages/applications.nix
+++ home/packages/applications.nix
@@ -31,6 +31,7 @@
     # System Tools
     file-roller
     remmina
+    virt-manager
 
     # Security
     burpsuite
--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -71,5 +71,18 @@
       enable = true;
       autoPrune.enable = true;
     };
+    spiceUSBRedirection.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [pkgs.OVMF.fd];
+        };
+      };
+    };
   };
 }
