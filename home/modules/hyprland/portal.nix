ed47e170ecb90cf5fe847357767b2dedbef4b4e5
Nikita Mitasov <me@ch4og.com>
2025-07-25 19:09:08 +0300

    update portals

--- home/modules/hyprland/portal.nix
+++ home/modules/hyprland/portal.nix
@@ -1,9 +1,14 @@
 {pkgs, ...}: {
   xdg.portal = {
-    extraPortals = [pkgs.xdg-desktop-portal-gtk];
-    config.common = {
-      default = ["hyprland" "gtk"];
-      "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+    extraPortals = [pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-wlr];
+    config = {
+      hyprland = {
+        default = ["hyprland" "gtk"];
+        "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+      };
+      common = {
+        default = ["wlr" "gtk"];
+      };
     };
   };
 }
